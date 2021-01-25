6ee7b854e046c89ee0685b55e084a6c2c15f8e98
MURAT KAYA <kaya_murat59@hotmail.com>
2021-01-25 18:13:13 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {  /*Müşteri operasyonları için  Console.WriteLine (); ile similasyon */
+        public void Add(Customer customer)
+        {
+             Console.WriteLine("Müştri eklendi");
+        
+        }
+        public void delete(Customer customer)
+        {
+
+            Console.WriteLine("Müşteri Silindi");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.TcNo="11111111111";
+            customer.Name = "Murat";
+            customer.LastName = "KAYA";
+
+            Customer customer1 = new Customer();
+            customer1.Id = 2;
+            customer1.TcNo = "22222222222";
+            customer1.Name = "Mahmut";
+            customer1.LastName = "GELE";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.delete(customer);
+            customerManager.Update(customer);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
